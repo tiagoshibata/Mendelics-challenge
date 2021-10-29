7adea690822a3cca7b4bc81828f0c420269eaf9c
Tiago Koji Castro Shibata <tiago.shibata@gmail.com>
2021-10-29 16:18:45 -0700

    Add README.md and more filtering

---
+++ dia_2/README.md
@@ -0,0 +1,9 @@
+# Solução do desafio Mendelics da LBB [(link)](https://github.com/mendelics/lbb-mendelics-2021/blob/main/Dia_2/README.md)
+
+As tarefas foram implementadas em Bash e C++. As mesmas dependências da [tarefa 1](../README.md) são usadas.
+
+## Tarefa 1
+
+> Quais variantes deverão ser desconsideradas no seu VCF? - Qualquer métrica do software de escolha poderá ser utilizada. Discorra sobre a métrica utilizada.
+
+
--- dia_2/clean-run.sh
+++ dia_2/clean-run.sh
@@ -2,8 +2,8 @@
 set -e
 cd "$(dirname "$0")"/..
 
-# echo 'Cleaning repository'
-# git clean -dfx
+echo 'Cleaning repository'
+git clean -dfx
 
 echo 'Indexing reference genome...'
 bowtie2-build dataset/grch38.chr22.fasta.gz index/grch38.chr22
@@ -12,17 +12,20 @@ echo 'Recompressing with bgzip...'
 zcat dataset/grch38.chr22.fasta.gz | bgzip > dataset/grch38.chr22.fasta.bgz.gz
 
 echo 'Mapping to reference genome...'
-bowtie2 -D200 --very-sensitive -D200 -p 24 -x index/grch38.chr22 -1 dataset/amostra-lbb_R1.fq.gz -2 dataset/amostra-lbb_R2.fq.gz > mapped_day2.sam
+bowtie2 -D200 --very-sensitive -D200 -p 24 -x index/grch38.chr22 -1 dataset/amostra-lbb_R1.fq.gz -2 dataset/amostra-lbb_R2.fq.gz --no-unal > mapped_day2.sam
 
-echo 'Filtering mappings with low quality...'
-pypy3 filter.py < mapped_day2.sam > filtered.sam
+# echo 'Filtering mappings with low quality...'
+# pypy3 filter.py < mapped_day2.sam > filtered.sam
 
 echo 'Converting to BAM...'
-samtools sort filtered.sam -o filtered.bam
-bcftools mpileup -q20 -Ou -f dataset/grch38.chr22.fasta.bgz.gz filtered.bam | bcftools call -mv -Oz -o amostra-lbb-day2.vcf.gz
+samtools sort mapped_day2.sam -o mapped_day2.bam
+sambamba view -h -t 24 -f bam -F '[XS] == null' mapped_day2.bam > filtered_day2.bam
+
+echo 'Creating VCF...'
+bcftools mpileup -q30 -Ou -f dataset/grch38.chr22.fasta.bgz.gz filtered_day2.bam | bcftools call -mv -Oz -o amostra-lbb-day2.vcf.gz
 
 echo 'Limiting to capture regions...'
 tabix -p vcf amostra-lbb-day2.vcf.gz
-tabix -R dataset/coverage.bed amostra-lbb-day2.vcf.gz > amostra-lbb-day2-capture-regions.vcf
+tabix -R dataset/coverage.bed.gz amostra-lbb-day2.vcf.gz > amostra-lbb-day2-capture-regions.vcf
 bgzip amostra-lbb-day2-capture-regions.vcf
 # bcftools mpileup -C10 -Ou -f dataset/grch38.chr22.fasta.bgz.gz mapped.bam | bcftools call -mv -Oz -o amostra-lbb.vcf.gz
